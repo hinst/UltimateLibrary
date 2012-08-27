0107d26b045f45dec59e5ad0d2528427ff0046fc
Alexander Savinoff <hinstance@yandex.ru>
2012-08-27 14:50:17 +0400

    Angle360 unit improved

--- Math/Angle360.pas
+++ Math/Angle360.pas
@@ -16,10 +16,14 @@ type
     fValue: single;
   public
     property Value: single read fValue write fValue;
+    procedure Random;
     procedure Assign(const aX: single);
     procedure AssignTo(var aX: single);
     procedure Inc(const aX: single);
     procedure Refresh;
+    procedure MoveToDesiredAngle(
+      const aDesiredAngle: TAngle360;
+      const aDelta: single);
   end;
 
 operator := (const aX: single): TAngle360;
@@ -27,6 +31,8 @@ operator := (const aX: single): TAngle360;
 operator := (const aX: TAngle360): single;
 
 operator = (const A, B: TAngle360): boolean;
+
+function MostCloseAngleDirection(const aCurrent, aDesired: TAngle360): shortint;
 
 implementation
 
@@ -45,7 +51,35 @@ begin
   result := A.Value = B.Value;
 end;
 
+function MostCloseAngleDirection(const aCurrent, aDesired: TAngle360): shortint;
+var
+  current, desired: single;
+  positiveDistance, negativeDistance: single;
+begin
+  current := aCurrent;
+  desired := aDesired;
+  if current <= desired then
+  begin
+    positiveDistance := desired - current;
+    negativeDistance := 360 - desired + current;
+  end
+  else
+  begin
+    positiveDistance := 360 - current + desired;
+    negativeDistance := current - desired;
+  end;
+  if positiveDistance < negativeDistance then
+    result := +1
+  else
+    result := -1;
+end;
+
 { TAngle360 }
+
+procedure TAngle360.Random;
+begin
+  Value := System.Random(360);
+end;
 
 procedure TAngle360.Assign(const aX: single);
 begin
@@ -69,6 +103,19 @@ begin
     fValue -= 360;
   while fValue < 0 do
     fValue += 360;
+end;
+
+procedure TAngle360.MoveToDesiredAngle(const aDesiredAngle: TAngle360;
+  const aDelta: single);
+var
+  d1, d2: shortint;
+begin
+  if self = aDesiredAngle then exit;
+  d1 := MostCloseAngleDirection(self, aDesiredAngle);
+  self.Inc( aDelta * d1 );
+  d2 := MostCloseAngleDirection(self, aDesiredAngle);
+  if d1 <> d2 then
+    self := aDesiredAngle;
 end;
 
 end.
